3e73a43975b6c583902954b1e3f14f25a113afb6
yeshrajawat <yeshrajawat@gmail.com>
2022-05-14 03:31:21 +0530

    Generating uniqueId using uuid library

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.scss';
+import { v4 as uuidv4 } from 'uuid';
 const Home = () => {
+
+  const [roomId,setRoomId] = useState("");
+  const [username,setUserName] = useState("");
+  const createNewRoom = (e)=> {
+    e.preventDefault();
+
+    const id = uuidv4();
+    setRoomId(id);
+    console.log(id);
+  }
+
+
   return (
     <div className='homePageWrapper' >
         <div className="formWrapper">
@@ -14,14 +27,21 @@ const Home = () => {
             <div className="middle">
               <div className="form">
                 <form action="">
-                  <input type="text" placeholder='ROOM ID'/>
-                  <input type="text" placeholder='USERNAME'/>
-                  <button type='SUBMIT'>Join</button>
+                  <input type="text" placeholder='ROOM ID' 
+                  onChange={(e)=> setRoomId(e.target.value)}
+                  value={roomId}/>
+
+                  <input type="text" placeholder='USERNAME'
+                  onChange={(e)=> setUserName(e.target.value)}
+                  value={username}/>
+                  
+                  <button type='submit' >Join</button>
                 </form>
               </div>
             </div>
             <div className="bottom">
-              <h4> If you don't have an invite then create <span>new room</span> </h4>
+              <h4> If you don't have an invite then create 
+                <span onClick={(e)=> createNewRoom(e)}> &nbsp;new room</span> </h4>
             </div>
         </div>
     </div>

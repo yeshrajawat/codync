ab3c6112462ea0f214d9bf38689de81d48721a6f
yeshrajawat <yeshrajawat@gmail.com>
2022-05-14 02:06:48 +0530

    Created react app for new project Codsync

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hemlo World</h1>
+    </div>
+  );
+}
+
+export default App;

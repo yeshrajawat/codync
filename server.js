a51ef2b093b452299a6f2ee38775a1ad1bfcfaf8
yeshrajawat <yeshrajawat@gmail.com>
2022-05-30 00:49:53 +0530

    Added Static as build version

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const http = require('http');
 const {Server} = require('socket.io');
 const ACTIONS = require('./src/actions');
+const path = require('path');
 const app  = express();
 const PORT = 5000;
 
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(express.static('build'));
+
+app.use((req,res,next) => {
+    res.sendFile(path.join(__dirname,'build','index.html'));
+})
+
 
 const userSocketMap = {}
 
